e7ec1069390addc277196ef955297e86ef3c645f
Cherifi Imane <50464518+CherifiImene@users.noreply.github.com>
2021-11-19 10:57:25 +0100

    submitting the data exploration homework

---
+++ Unit2/Homeworks/data_exploration_hw.py
@@ -0,0 +1,17 @@
+#  Data Exploration Homework
+
+#From the previously seen dataset we want to do a univariate analysis on the variable “EU_Sales” 
+# create a notebook 
+# and use Pandas to extract information about this variable 
+# and answer the following questions:
+
+#    Calculate the following quantities:
+#        Mean
+#        Median
+#        Mode
+#        Variance
+#        Standard deviation
+#    Plot the following figures :
+#        Scatter plot
+#        Histogram
+#        Box plot (look it up yourself to find out what it is)
