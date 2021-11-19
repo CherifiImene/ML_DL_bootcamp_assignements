4f31360e0358c4b03d18172093a501ebea677524
Cherifi Imane <50464518+CherifiImene@users.noreply.github.com>
2021-11-19 10:58:12 +0100

    Create preparatory_unit_project.py

---
+++ Unit1/Project/preparatory_unit_project.py
@@ -0,0 +1,32 @@
+# Project Overview:
+#
+#There are many cases when as data scientists we need to find the best curve’s equation that can fit a given dataset or sample. 
+# That is said you have some data points and you want to know what is the curve that can modelise them the best. This is known as the “Curve fitting problem”.
+#
+#In this project we will work you through an example of this problem.
+#
+#This project is divided into sections where in each section you will be exercising to use one of the fundamental concepts we saw in this first unit.. 
+# From OOP, to SQL to Linear Algebra to the final Machine learning workflow lesson.
+#
+#You will have enough guidance throughout the project sections and this project will be graded and reviewed by a teacher assistant 
+# as well as the chance for you to reach out to the TA via slack whenever you feel you are stuck.
+#
+#The key is to know that the best way to learn is the hardest! :)
+
+
+
+#Details:
+
+#We will be using a special dataset called “Longley's Economic Regression Data” and we will study the problem step by step:
+#
+#    You will need to create a new environment for your project including all necessary libraries installed in it. 
+#    Also Keep in my mind that it’s always a good practice to use a Jupyter notebook as a tool to experiment and not to write the whole project’s code in a notebook. 
+#    This will add visibility and organization to what you do.
+#
+#    First section is about fetching your data: 
+#           your data are provided to you in the file longley.db in a SQLlite database format, 
+#           your first mission is to read this database and retrieve your data.
+#
+#    Second section is about how to manage the data types and variable types.
+#
+#    Then there is the linear Algebra part where you get to learn how to make a working curve fitting example.
